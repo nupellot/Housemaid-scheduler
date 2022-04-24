47e36fe970c5e7b8b514b316fdc9d1a5e836de47
Dmitriy Korotkov <nupellot@yahoo.com>
2022-04-25 01:24:53 +0300

    Новый алгоритм поиска номера недели + новый порядок

--- script2.js
+++ script2.js
@@ -1,42 +1,29 @@
-// alert("ass1");
-$('button:nth-child(1)').on("click", function(){
-  $('.wrapper').css("transform", "");
-});
-$('button:nth-child(2)').on("click", function(){
-  $('.wrapper').css("transform", "rotateY(-90deg)");
-});
-$('button:nth-child(3)').on("click", function(){
-  $('.wrapper').css("transform", "rotateY(90deg)");
-});
-$('button:nth-child(6)').on("click", function(){
-  $('.wrapper').css("transform", "rotateY(180deg)");
-});
-
-// var now = new Date();
-// alert( now );
 currentdate = new Date();
-var oneJan = new Date(currentdate.getFullYear(),0,1);
-var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-var weekNumber = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
-// alert(result);
-// weekNumber = 14;
-// alert(weekNumber);
 
-switch (weekNumber % 4) {
+
+// alert(getYearWeek(currentdate));
+
+switch (getYearWeek(currentdate) % 4) {
   case 0:
-    // alert(1);
     $('.wrapper').css("transform", "");
     break;
   case 1:
-    // alert(2);
     $('.wrapper').css("transform", "rotateY(-90deg)");
     break;
   case 2:
-    // alert(3);
     $('.wrapper').css("transform", "rotateY(90deg)");
     break;
   case 3:
-    // alert(4);
     $('.wrapper').css("transform", "rotateY(180deg)");
     break;
+}
+
+function getYearWeek(date){
+	var date2=new Date(date.getFullYear(), 0, 1);
+	var day1=date.getDay();
+	if(day1==0) day1=7;
+	var day2=date2.getDay();
+	if(day2==0) day2=7;
+	d = Math.round((date.getTime() - date2.getTime()+(day2-day1)*(24*60*60*1000)) / 86400000);  
+	return Math.ceil(d /7)+1; 
 }
