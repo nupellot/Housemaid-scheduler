3184c6272c4eb3148b3985826a079cb944581465
Dmitriy Korotkov <nupellot@yahoo.com>
2022-05-11 11:20:30 +0300

    Новый порядок: Stassik -> Dima K -> Ivan -> Dima N

---
+++ script.js
@@ -0,0 +1,28 @@
+currentdate = new Date();
+
+// alert(getYearWeek(currentdate));
+weekNumber = getYearWeek(currentdate);
+switch (weekNumber % 4) {
+  case 0:
+    $('.wrapper').css("transform", "");  // Dima N
+    break;
+  case 1:
+    $('.wrapper').css("transform", "rotateY(-90deg)");  // Stassik
+    break;
+  case 2:
+    $('.wrapper').css("transform", "rotateY(90deg)");  // Dima K
+    break;
+  case 3:
+    $('.wrapper').css("transform", "rotateY(180deg)");  // Ivan
+    break;
+}
+
+function getYearWeek(date){
+	var date2=new Date(date.getFullYear(), 0, 1);
+	var day1=date.getDay();
+	if(day1==0) day1=7;
+	var day2=date2.getDay();
+	if(day2==0) day2=7;
+	d = Math.round((date.getTime() - date2.getTime()+(day2-day1)*(24*60*60*1000)) / 86400000);
+	return Math.ceil(d /7)+1;
+}
